64a4741cdff4768e712f90cb0805c1e1874e84b1
CLEMENS KRAMETTER <clemens.krametter@student.tugraz.at>
2018-11-02 17:33:18 +0100

    Changed Books.js select menu

--- src/components/Book.js
+++ src/components/Book.js
@@ -14,7 +14,7 @@ class Book extends Component {
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
             <div className="book-shelf-changer">
               <select onChange={this.changeBookShelf} value = {book.shelf}>
-                <option value="none" disabled>Move to...</option>
+                <option disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
                 <option value="read">Read</option>
